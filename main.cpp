fc0273fc9dca364edafbed5341237ca3e8eaa314
Avital <avital.bor@campus.technion.ac.il>
2020-05-14 23:17:35 +0300

    fix bags in avl

--- main.cpp
+++ main.cpp
@@ -1,21 +1,47 @@
-#include <iostream>
-#include "AvlTree.h"
-#include <cassert>
+/*
+ * testAVLTree.cpp
+ *
+ *  Created on: 19 ���� 2013
+ *      Author: shirel
+ */
+
+#include "AVLTree.h"
+#include <vector>
+#include <ostream>
+#include <random>
+#include <chrono>
+#include <algorithm>
 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    //AVL_Basics();
+#define addNode insert
+#define removeNode remove
 
-    int arr[9];
-    for(int i=0;i<9;i++){
-        arr[i]=i;
+
+int main(){
+    AvlTree<int, int> tree;
+
+    std::vector<int> vector;
+    for (int i=1; i<=20; i++) vector.push_back(i);
+           printf("i\n");
+
+    //Randomly insert and removes nodes
+    for (int k = 0; k < 20; ++k) {
+        printf("%d\n",k);
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        shuffle (vector.begin(), vector.end(), std::default_random_engine(seed));
+        for (std::vector<int>::iterator it = vector.begin() ; it != vector.end(); ++it){
+            tree.addNode(*it,*it);
+
+        }
+        //tree.printTree();
+        shuffle (vector.begin(), vector.end(), std::default_random_engine(seed));
+        for (std::vector<int>::iterator it = vector.begin() ; it != vector.end(); ++it){
+            tree.removeNode(*it);
+            //tree.printTree();
+
+        }
+        tree.clear();
     }
-    AvlTree <int ,int> sari(arr,arr,9);
-    printf("gello");
-
-
-
-
     return 0;
 }
+
